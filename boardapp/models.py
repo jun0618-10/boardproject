4424ebeab7c1f2169ea8b92b5f6e3bb05060c46d
jun0618-10 <lingmucong84@gmail.com>
2024-03-01 23:17:57 +0900

    完成

--- boardapp/models.py
+++ boardapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BoardModel(models.Model):
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    author = models.CharField(max_length = 50)
+    snsimage = models.ImageField('')
+    good = models.IntegerField(null=True,blank=True,default=1)
+    read = models.IntegerField(null=True,blank=True,default=1)
+    read_text = models.TextField(null=True,blank=True,default='a')
+
+
+
--- boardproject/settings.py
+++ boardproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "boardapp.apps.BoardappConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = "boardproject.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [ BASE_DIR/ 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,9 +116,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "sta/"
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
+MEDIA_ROOT = BASE_DIR/'media'
+
+STATICFILES_DIRS =[ str(BASE_DIR/'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = 'medi/'
+
+LOGIN_URL = 'login'
